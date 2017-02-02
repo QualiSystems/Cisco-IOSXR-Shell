f43fd83e1108d4c4c64bb2a49cb4a0f99e3f6cc6
CoYe <wiseman.kostya@gmail.com>
2017-02-02 13:11:47 +0200

    fixed save flow

--- package/cloudshell/networking/cisco/iosxr/runners/cisco_ios_xr_configuration_runner.py
+++ package/cloudshell/networking/cisco/iosxr/runners/cisco_ios_xr_configuration_runner.py
@@ -1,3 +1,4 @@
+from cloudshell.networking.cisco.flow.cisco_save_flow import CiscoSaveFlow
 from cloudshell.networking.cisco.iosxr.cisco_ios_xr_cli_handler import CiscoIOSXRCliHandler
 from cloudshell.networking.cisco.iosxr.flows.cisco_ios_xr_restore_flow import CiscoIOSXRRestoreFlow
 from cloudshell.networking.cisco.runners.cisco_configuration_runner import CiscoConfigurationRunner
@@ -7,5 +8,7 @@ class CiscoIOSXRConfigurationRunner(CiscoConfigurationRunner):
     def __init__(self, cli, logger, context, api):
         super(CiscoIOSXRConfigurationRunner, self).__init__(cli, logger, context, api)
         self._cli_handler = CiscoIOSXRCliHandler(cli, context, logger, api)
-        self._restore_flow = CiscoIOSXRRestoreFlow(self._cli_handler, self._logger)
+        self._save_flow = CiscoSaveFlow(cli_handler=self._cli_handler,
+                                        logger=self._logger)
+        self._restore_flow = CiscoIOSXRRestoreFlow(cli_handler=self._cli_handler, logger=self._logger)
         self.file_system = "bootflash:"
